f70b9bea4a7d4ba051ca533752870d16039aec64
Ravi Ranjan <raviranjan1140@gmail.com>
2022-03-07 21:35:07 +0530

    index js main file added

---
+++ Calculator/index.js
@@ -0,0 +1,8 @@
+const add= require("./add.js")
+add(3,4);
+const subs= require("./substract.js")
+subs(5,3)
+const multi= require("./multiply.js")
+multi(5,5)
+const divide=require("./divide.js")
+divide(25,5)
