de137d6f7a5991fc0d7b48ba0ab6f89f3ff2a4ad
Ravi Ranjan <raviranjan1140@gmail.com>
2022-03-27 10:49:31 +0530

    index js updated

--- Express RealtionShip/index.js
+++ Express RealtionShip/index.js
@@ -13,305 +13,151 @@ const connectDB=()=>{
 }
 
 
-const userSchema = new mongoose.Schema(
+// Schema for section
+
+
+const sectionSchema = new mongoose.Schema({
+    "sectionName":{type:String,required:true},
+});
+
+
+
+//  Connecting the schema to section collection
+
+
+const Section = mongoose.model("section", sectionSchema);
+
+
+
+//   Schema for books
+
+const bookSchema = new mongoose.Schema({
+    "sectionId":{type:mongoose.Schema.Types.ObjectId,ref:"section",required:true},
+   "name":{type:String,required:true},
+   "body":{type:String,required:true},
+})
+
+
+//  Connecting the schema to books collections
+
+const Book = mongoose.model("book", bookSchema);
+
+
+// Schema for author
+
+const authorSchema = new mongoose.Schema({
+    "authorName":{type:String,required:true},
+})
+
+// Connecting the schema to authors collections
+
+const Author = mongoose.model("author", authorSchema)
+
+
+// Schema for book author
+
+const bookAuthorSchema = new mongoose.Schema({
+    "bookId":{type:mongoose.Schema.Types.ObjectId, ref:"book", required:true},
+    "authorId":{type:mongoose.Schema.Types.ObjectId,ref:"author", required:true},
+})
+//Connecting the Schema to bookAuthors collection
+
+const BookAuthor = mongoose.model("bookAuthor", bookAuthorSchema)
+
+//checkedout schema
+
+const checkedOutSchema=new mongoose.Schema(
     {
-      firstName: { type: String, required: true },
-      lastName: { type: String, required: false },
-      email: { type: String, required: true, unique: true },
-      password: { type: String, required: true },
+       
+        "checkOutTime":{type:Number, required:true},
+        "checkInTime":{type:Number, required:true},
+    
     },
-    {
-      versionKey: false,
-      timestamps: true, 
-    }
-  );
-  
-// Step 2 : creating the model
-const User = mongoose.model("detail", userSchema);
-
-// POST schema
-const postSchema= new mongoose.Schema({
- title:{type:String,required:true},
- body:{type:String,required:true},
- userId:{type:mongoose.Schema.Types.ObjectId,ref:"detail",require:true},
-
-},{
-    timestamps:true,
-});
-
-
-const post= mongoose.model("post",postSchema);
-
-
-// commentSchema
-
-const commentSchema= new mongoose.Schema({
-    body:{type:String,required:true},
-    postId:{type:mongoose.Schema.Types.ObjectId,ref:"post",required:true},
-    userId:{type:mongoose.Schema.Types.ObjectId,ref:"detail",require:true},
-
-   },{
-       timestamps:true,
-   });
    
-   
-   const comment= mongoose.model("comment",commentSchema);
-
-
-
-// createing the uers crud
-app.get(("/users"),async(req,res)=>{
-    try {
-
-        const userdata=await User.find({}).lean().exec();
-        return res.status(201).send(userdata);
-        
-    } catch (error) {
-
-        return res.status(500).send("something went wrong");
+    );
+    //model for checkedout
     
-    }
+    const CheckedOut = mongoose.model("checkout",checkedOutSchema);
 
+///CRUD API for Section
+
+app.post("/sections", async (req, res)=>{
+    const sec = await Section.create(req.body)
+
+    return res.status(201).send({sec})
+});
+
+app.get("/sections", async (req,res)=>{
+    const sec = await Section.find().lean().exec();
+    res.status(200).send({sec})
 });
 
 
 
+//CRUD API for Books
 
-app.post("/users",async(req,res)=>{
-    try {
 
-        const user=await User.create(req.body)
+app.post("/books", async (req, res)=>{
+    const book = await Book.create(req.body)
 
-        console.log(req.body)
-        return res.status(201).send(user);
-        
-    } catch (error) {
+    return res.status(201).send({book})
+});
 
-        return res.status(500).send(error.message);
-    
-    }
-
+app.get("/books", async (req,res)=>{
+    const book = await Book.find().populate("sectionId").lean().exec()
+    res.status(200).send({book})
 });
 
 
-// finding single one with ID
-
-app.get(("/users/:ID"),async(req,res)=>{
-    try {
-
-        const user=await User.findById(req.params.ID).lean().exec()
-        return res.status(201).send(user);
-        
-    } catch (error) {
-
-        return res.status(500).send(error.message);
-    
-    }
-
-});
-
-// updating the values
-
-app.patch(("/users/:ID"),async(req,res)=>{
-    try {
-
-        const user=await User.findByIdAndUpdate(req.params.ID,req.body,{new:true}).lean().exec()
-        return res.status(201).send(user);
-        
-    } catch (error) {
-
-        return res.status(500).send(error.message);
-    
-    }
-
-});
-
-// deleting the user
-
-app.delete(("/users/:ID"),async(req,res)=>{
-    try {
-
-        const user=await User.findByIdAndDelete(req.params.ID).lean().exec();
-        return res.status(201).send(user);
-        
-    } catch (error) {
-
-        return res.status(500).send(error.message);
-    
-    }
-
+app.get("/books/:id", async(req, res)=>{
+    const book = await Book.findById(req.params.id).lean().exec();
+    res.status(200).send({book})
 });
 
 
 
-// POST CRUD
+//CRUD API for Authors
 
 
-app.get(("/posts"),async(req,res)=>{
-    try {
+app.post("/authors", async (req, res)=>{
+    const author = await Author.create(req.body)
 
-        const postdata=await post.find().populate({path:"userId",select:{first_name: 1,email: 1,_id: 0}}).lean().exec();
+    return res.status(201).send({author})
+});
 
-        return res.status(201).send(postdata);
-        
-    } catch (error) {
-
-        return res.status(500).send(error.message);
-    
-    }
-
+app.get("/authors", async (req,res)=>{
+    const author = await Author.find().lean().exec();
+    res.status(200).send({author})
 });
 
 
-app.post(("/posts"),async(req,res)=>{
-    try {
-        
-        const userpost=await post.create(req.body)
-        return res.status(201).send(userpost);
-        
-    } catch (error) {
-        
-        return res.status(500).send(error.message);
-        
-    }
-    
+//CRUD API for bookAuthor
+
+app.post("/bookauthors", async(req,res)=>{
+    const bookauthor = await BookAuthor.create(req.body);
+    return res.status(201).send({bookauthor})
+})
+app.get("/bookauthors", async (req,res)=>{
+    const bookauthor = await BookAuthor.find().populate("authorId").populate("bookId").lean().exec();
+    res.status(200).send({bookauthor})
 });
 
 
-app.post(("/posts/:id"),async(req,res)=>{
-    try {
 
-        const postfind=await post.findById(req.params.id).lean().exec();
-        return res.status(201).send(postfind);
-        
-    } catch (error) {
+// All books written by an Author
 
-        return res.status(500).send(error.message);
-    
-    }
+app.get("/bookauthors/:id", async(req, res)=>{
+    const bookauthors = await BookAuthor.find({authorId:req.params.id}).lean().populate("bookId").exec();
+    res.send(bookauthors)
+})
 
-});
+// searching books in a section
 
-// update the post
+app.get("/books/:id", async(req,res)=>{
+    const books = await Book.find({sectionId:req.params.id}).lean().populate("sectionId").exec();
+    res.send(books)
+})
 
-app.post(("/posts/:id"),async(req,res)=>{
-    try {
-
-        const postupdate=await post.findByIdAndUpdate(req.params.id,{new:true}).lean().exec();
-        return res.status(201).send(postupdate);
-        
-    } catch (error) {
-
-        return res.status(500).send(error.message);
-    
-    }
-
-});
-
-// delete post
-
-app.delete(("/posts/:id"),async(req,res)=>{
-    try {
-
-        const postdelete=await post.findByIdAndDelete(req.params.id).lean().exec();
-        return res.status(201).send(postdelete);
-        
-    } catch (error) {
-
-        return res.status(500).send(error.message);
-    
-    }
-
-});
-
-app.get(("/posts/:postId/comments"),async(req,res)=>{
-    try {
-
-        const comments=await comment.find(req.params.postId).lean().exec();
-        return res.status(201).send(comments);
-        
-    } catch (error) {
-
-        return res.status(500).send(error.message);
-    
-    }
-
-});
-
-// comment crud
-
-app.get(("/comments"),async(req,res)=>{
-
-    try {
-        
-        const comments=await comment.find({}).populate({path:"postId",select:["title"],populate:{path:"userId",select:["email"]},})
-        .populate({path:"userId",select:["email"]}).lean().exec();
-    
-        return res.status(201).send(comments)
-    } catch (error) {
-        return res.send(error.message)
-    }
-});
-
-
-app.post(("/comments"),async(req,res)=>{
-
-    try {
-        
-        const comments=await comment.create(req.body);
-    
-        return res.status(201).send(comments)
-    } catch (error) {
-        return res.send(error.message)
-    }
-});
-
-// get comment by id
-
-
-app.get(("/comments/:id"),async(req,res)=>{
-
-    try {
-        
-        const comments=await comment.findById(req.params.id)
-        .populate({path:"postId",select:["title","body"],populate:{path:"userId",select:["password"]},})
-        .populate({path:"userId",select:["email"]}).lean().exec();
-    
-        return res.status(201).send(comments)
-    } catch (error) {
-        return res.send(error.message)
-    }
-});
-
-// update the comment
-
-app.patch(("/comments/:id"),async(req,res)=>{
-
-    try {
-        
-        const comments=await comment.findByIdAndUpdate(req.params.id,req.body,{new:true})
-        .populate({path:"postId",select:["title"],populate:{path:"userId",select:["email"]},})
-        .populate({path:"userId",select:["email"]}).lean().exec();
-    
-        return res.status(201).send(comments)
-    } catch (error) {
-        return res.send(error.message)
-    }
-});
-
-// delete the comment
-
-app.delete(("/comments/:id"),async(req,res)=>{
-
-    try {
-        
-        const comments=await comment.findByIdAndDelete(req.params.id).lean().exec();
-    
-        return res.status(201).send(comments)
-    } catch (error) {
-        return res.send(error.message)
-    }
-});
 
 
 app.listen(4100,async()=>{
